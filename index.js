6aba6aeb180b4f67ca2097567e12269b0198fc9b
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2017-12-01 02:32:04 -0200

    Adicionados arquivos principais

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * edb-node-chrome
+ *
+ * Parte do Projeto:
+ * https://github.com/digitalbocca/edb-node-chrome
+ *
+ * @file index.js
+ * @namespace root
+ * @description Arquivo principal da aplicação.
+ * @since v0.1.0
+ *
+ * @copyright (c)2017 - Estúdio Digital Bocca - https://estudiodigitalbocca.com.br/
+ * @author Gabriel Bertola Bocca - gabriel at estudiodigitalbocca.com.br
+ *
+ * @version v1.0.0
+ */
+
+const chromeLauncher = require('chrome-launcher')
+
+chromeLauncher.launch({
+  startingUrl: 'https://estudiodigitalbocca.com.br'
+}).then(chrome => {
+  console.log(`Navegador rodando na porta ${chrome.port}`)
+})
