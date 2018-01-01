6107b294e1c6ca5415766dc8e6d8cbb92ef7fece
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2018-01-01 16:21:20 -0200

    Atualização de dependências

---
+++ README.md
@@ -0,0 +1,17 @@
+# Estúdio Digital Bocca
+
+## edb-node-chrome
+
+### v0.2.0
+
+> Abre o navegador Chrome a partir do node.
+
+## CHANGELOG
+
+### v0.2.0
+
+- Atualização de dependências.
+
+### v0.1.0
+
+- Adicionados arquivos principais.
--- index.js
+++ index.js
@@ -11,10 +11,10 @@
  * @description Arquivo principal da aplicação.
  * @since v0.1.0
  *
- * @copyright (c)2017 - Estúdio Digital Bocca - https://estudiodigitalbocca.com.br/
+ * @copyright (c)2017-2018 - Estúdio Digital Bocca - https://estudiodigitalbocca.com.br/
  * @author Gabriel Bertola Bocca - gabriel at estudiodigitalbocca.com.br
  *
- * @version v1.0.0
+ * @version v1.1.0
  */
 
 const chromeLauncher = require('chrome-launcher')
